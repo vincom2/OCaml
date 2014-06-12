cbabe9202c1cf1e11d8c631ccb5f49d731eb71b4
Vincent Huang <vincom2@gmail.com>
2014-06-12 16:55:50 -0400

    Some silly min-cut stuff using Karger's algorithm Returns the *number* of edges in the min cut w.h.p. Doesn't actually return the min cut itself... yet?

---
+++ karger/contractable.ml
@@ -0,0 +1,98 @@
+(* uses imperative graphs from ocamlgraph *)
+(* graphs and edges are integer-labelled because I don't care enough *)
+(* this is a crappy implementation that doesn't track any of the
+ * original "properties" of the vertices or edges
+ * because all I am trying to do is establish the _size_
+ * of the min cut *)
+(* assumes the original graph provided is simple, connected and undirected
+ * and every edge label is unique *)
+open Core.Std
+
+module type COUNTER_GRAPH = sig
+  include Graph.Sig_pack.S
+
+  val init_vertex_counter : int -> unit
+  val init_edge_counter : int -> unit
+
+  val get_vertex_label : unit -> int
+  val get_edge_label : unit -> int
+end
+
+module Make(G : COUNTER_GRAPH) = struct
+  include G
+
+  exception TooSmall
+
+      Random.self_init ()
+
+  let contract g l =
+    let consider e =
+      let u = E.src e
+      and v = E.dst e in
+      if E.label e = l then
+        let v_new = V.create (get_vertex_label ()) in
+        (* these could be combined, I suppose, but whatever *)
+        let f_u e =
+          let l = E.label e in
+          let tmp = E.src e in
+          let u' = if tmp = u then E.dst e else tmp in
+          if u' = v then () else begin
+            remove_edge_e g e;
+            add_edge_e g (E.create u' l v_new)
+          end
+        in
+        let f_v e =
+          let l = E.label e in
+          let tmp = E.src e in
+          let v' = if tmp = v then E.dst e else tmp in
+          if v' = u then () else begin
+            remove_edge_e g e;
+            add_edge_e g (E.create v' l v_new)
+          end
+        in
+        add_vertex g v_new;
+        iter_succ_e f_u g u;
+        iter_succ_e f_v g v;
+        remove_vertex g u;
+        remove_vertex g v
+      else ()
+    in
+    iter_edges_e consider g
+
+  let cut g' =
+    let g = copy g' in
+    if nb_vertex g < 2 then raise TooSmall else
+      while nb_vertex g > 2 do
+        let r = Random.int (nb_edges g) in
+        (* Printf.printf "%i vertices left, contracting edge %i\n" (nb_vertex g) r; *)
+        contract g r
+      done;
+    (g, nb_edges g)
+
+  let prob_min_cut g =
+      let min_cut = ref Int.max_value
+      and min_g = ref g in
+      let n = float (nb_vertex g) in
+      let stop = Int.of_float ((n**2.) *. (log n)) + 1 in
+      for i = 0 to stop do
+        let (cut_graph, cut_size) = cut g in
+        if cut_size < !min_cut then begin
+          min_cut := cut_size;
+          min_g := cut_graph
+        end
+        else ()
+      done;
+      (!min_g, !min_cut)
+
+  let parse_dot_file filename =
+    let g = parse_dot_file filename in
+    let do_stuff e =
+      let u = E.src e
+      and v = E.dst e in
+      G.remove_edge_e g e;
+      G.add_edge_e g (E.create u (get_edge_label ()) v)
+    in
+    iter_edges_e do_stuff g;
+    init_vertex_counter (G.nb_vertex g + 1);
+    g
+end
---
+++ karger/karger.ml
@@ -0,0 +1,56 @@
+open Core.Std
+
+(* oh shit, this isn't good at all if I want to have more than one graph
+ * created by this module D: *)
+(* TODO: CHANGE THIS *)
+module Counter_graph : Contractable.COUNTER_GRAPH = struct
+  include Graph.Pack.Graph
+
+  let vertex_counter = ref 0
+  let edge_counter = ref 0
+
+  let init_vertex_counter n =
+    vertex_counter := n
+
+  let init_edge_counter m =
+    edge_counter := m
+
+  let get_vertex_label () =
+    let curr = !vertex_counter in
+    vertex_counter := curr + 1;
+    curr
+
+  let get_edge_label () =
+    let curr = !edge_counter in
+    edge_counter := curr + 1;
+    curr
+end
+
+module C = Contractable.Make(Counter_graph)
+(* lol there actually isn't any point in producing output right now
+ * since I don't have a mapping from final edges to initial edges at all.
+ * Should "rectify" that... someday? *)
+let main infile ?outfile =
+  let test = C.parse_dot_file infile in
+  let () = C.display_with_gv test in
+  let (test',m) = C.prob_min_cut test in
+  let () = Printf.printf "w.h.p. a min cut of test.dot contains %i edge(s)\n" m in
+  let () = C.display_with_gv test' in
+  match outfile with
+  | None -> ()
+  | Some outfilename -> C.dot_output test' outfilename
+
+(* command line argument stuff *)
+let spec =
+  let open Command.Spec in
+  empty
+  +> anon ("filename" %: string)
+  +> flag "-o" (optional string) ~doc:"outfile Outputs the final \"graph\" to outfile"
+
+let command =
+  Command.basic
+    ~summary:"Run Karger's probabilistic min-cut algorithm on a graph"
+    spec
+    (fun infile outfile () -> main infile ?outfile)
+
+let () = Command.run ~version:"0.1" command
