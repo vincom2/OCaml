356f0b7624cc918d6a771bb5862d54add8e9165b
Vincent Huang <vincom2@gmail.com>
2014-06-12 17:43:46 -0400

    made an interface file for contractable

---
+++ karger/contractable.mli
@@ -0,0 +1,22 @@
+module type COUNTER_GRAPH = sig
+  include Graph.Sig_pack.S
+
+  val init_vertex_counter : int -> unit
+  val init_edge_counter : int -> unit
+
+  val get_vertex_label : unit -> int
+  val get_edge_label : unit -> int
+end
+
+module Make :
+  functor (G : COUNTER_GRAPH) ->
+    sig
+      include COUNTER_GRAPH
+
+      exception TooSmall
+      
+      (* val contract : t -> int -> unit *)
+      (* val cut : t -> t * int *)
+      val prob_min_cut : t -> t * int
+      val parse_dot_file : string -> t
+    end
